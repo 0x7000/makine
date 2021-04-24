7049e7788588c0bb18a985c6e392e6b1101ff411
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2021-04-25 02:01:52 +0300

    Added wiki search and write code

---
+++ ilteriskeskin/README.md
@@ -0,0 +1,8 @@
+**Install**
+
+```shell
+python3 -m venv env
+source env/bin/activate
+pip3 install requirements.txt
+python3 main.py
+```
--- ilteriskeskin/main.py
+++ ilteriskeskin/main.py
@@ -1,8 +1,28 @@
 import wikipedia
 
-def test():
-    print(wikipedia.search("Bill"))
+WORDLIST_FILE = "wordlist/test_wordlist.txt"
+WIKIPEDIA_RESULT_FILE = "wordlist/wiki_results.txt"
+
+
+def read_wordlist(wordlist_file):
+    with open(wordlist_file) as words_file:
+        words = words_file.readlines()
+        for i in words:
+            search_wikipedia(i)
+
+
+def search_wikipedia(word):
+    wikipedia.set_lang('tr')
+    with open(WIKIPEDIA_RESULT_FILE, "a") as wiki_result:
+        try:
+            page = wikipedia.page(word)
+            wiki_result.write(page.title)
+            wiki_result.write(":")
+            wiki_result.write(page.summary)
+            wiki_result.write("\n")
+        except:
+            print(word, ": not found")
 
 
 if __name__ == "__main__":
-    test()
+    read_wordlist(WORDLIST_FILE)
