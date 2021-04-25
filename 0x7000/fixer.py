251a3decf1e4f9e240549ee323111f00e3eee0e5
0x7000 <rekreb81@gmail.com>
2021-04-26 01:55:51 +0300

    renames

--- 0x7000/fixer.py
+++ 0x7000/fixer.py
@@ -6,18 +6,18 @@ def main():
     kelime = input(": ")
     kelimeler = open("data/wordlist.txt", "r")
     sozluk = kelimeler.readlines()
-    duzeltilmis = []
-    oneri = []
+    benzer = []
+    enyakin = []
     for i in sozluk:
         i = i.lower().strip("\n")
         oran = difflib.SequenceMatcher(None, i, kelime).ratio() * 100
-        if oran >= 80:
-            duzeltilmis.append(i)
+        if oran >= 75:
+            benzer.append(i)
             if i[0] == kelime[0]:
                 if len(i) == len(kelime):
-                    oneri.append(i)
-    print("Benzerleri: {}".format(duzeltilmis))
-    print("En yakın: {}".format(oneri))
+                    enyakin.append(i)
+    print("Benzerleri: {}".format(benzer))
+    print("En yakın: {}".format(enyakin))
 
 
 if __name__ == '__main__':
